5f6a170fbc5cf3364b2eb55f537f55db136d4811
Moussa TROIANO <motroian@made-f0Br3s12.clusters.42paris.fr>
2023-12-18 21:59:33 +0100

    pas marrant

--- CPP05/ex03/srcs/Intern.cpp
+++ CPP05/ex03/srcs/Intern.cpp
@@ -1,19 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: motroian <motroian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/16 23:10:23 by motroian          #+#    #+#             */
-/*   Updated: 2023/12/18 16:46:17 by motroian         ###   ########.fr       */
+/*   Created: 2023/12/18 21:24:25 by motroian          #+#    #+#             */
+/*   Updated: 2023/12/18 21:45:36 by motroian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "AForm.hpp"
+#include "Intern.hpp"
 
-int main ()
-{
-	Bureaucrat socrate("socrate", 13);
-	std::cout << socrate;
-}
