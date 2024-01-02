7b8077b9bf0e7dda7b960677b89806cb6603a249
Moussa TROIANO <motroian@bess-f1r5s2.clusters.42paris.fr>
2024-01-02 23:23:44 +0100

    retour des vac

---
+++ CPP08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: motroian <motroian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/02 20:58:52 by motroian          #+#    #+#             */
+/*   Updated: 2024/01/02 23:10:12 by motroian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #ifndef MUTANTSTACK_HPP
+// # define MUTANTSTACK_HPP
+
+// #include <cstdlib>
+// #include <vector>
+// #include <stack>
+// #include <list>
+// #include <algorithm>
+// #include <iostream>
+// #include <stdexcept>
+
+// template <typename T>
+
+// class MutantStack
+// {
+// 	public :
+// 		MutantStack();
+// 		MutantStack( MutantStack  const & rhs);
+// 		MutantStack &operator=( MutantStack const & rhs );
+// 		~MutantStack();
+// 	private :
+// 		std::stack<T> internalStack;
+// };
+
+// std::ostream &	operator<<( std::ostream & o, Span const & rhs);
+
+// #endif
