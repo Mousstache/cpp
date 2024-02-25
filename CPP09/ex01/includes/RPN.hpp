6b299ff4c27663cda2c09762a4adcabde862081a
Moussa TROIANO <motroian@bess-f2r5s14.clusters.42paris.fr>
2024-02-26 00:08:57 +0100

    cppey

---
+++ CPP09/ex01/includes/RPN.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: motroian <motroian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 19:44:11 by motroian          #+#    #+#             */
+/*   Updated: 2024/02/25 20:31:49 by motroian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN
+# define RPN
+
+#include <cstdlib>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sstream>
+
+// class RPN
+// {
+// 	public:
+// 		// RPN ();
+// 		// RPN ( RPN const &src );
+// 		// RPN &operator= ( RPN const &src );
+// 		// ~RPN ();
+
+// 		// void	calculate ( void );
+// 	private:
+// 		std::stack<int> Nb;
+// };
+
+#endif
---
+++ CPP09/ex01/main.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: motroian <motroian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 19:10:51 by motroian          #+#    #+#             */
+/*   Updated: 2024/02/26 00:08:39 by motroian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RPN.hpp"
+
+
+int calculate(std::string operation)
+{
+	std::stack<int> stack;
+		std::stringstream ss(operation);
+		std::string token;
+		while (ss >> token)
+		{
+			if (isdigit(token[0]))
+				stack.push(atoi(token.c_str()));
+			else
+			{
+				int nb2 = stack.top();
+				stack.pop();
+				std::cout << "number1: " << nb2 << std::endl;
+				int nb1 = stack.top();
+				stack.pop();
+				std::cout << "number2: " << nb1 << std::endl;
+				int result;
+				if (token == "-")
+				{
+					result = nb1 - nb2;
+				}
+				else if (token == "+")
+				{
+					result = nb1 + nb2;
+				}
+				else if (token == "/")
+				{
+					result = nb1 / nb2;
+				}
+				else if (token == "*")
+				{
+					result = nb1 * nb2;
+				}
+				stack.push(result);
+			}
+		}
+		return (stack.top());
+}
+
+int main (int ac, char **av)
+{
+	if (ac == 2)
+	{
+		int result = calculate(av[1]);
+		std::cout << result << std::endl;
+	}
+	else
+		std::cout << "Wrong args" << std::endl;
+}
